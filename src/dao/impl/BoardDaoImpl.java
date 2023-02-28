b3a6524b2b75d2a96a0100878ed781392921fd6b
zahir <zahir7@naver.com>
2023-03-01 00:41:17 +0900

    javafx

---
+++ src/dao/impl/BoardDaoImpl.java
@@ -0,0 +1,106 @@
+package dao.impl;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import dao.BoardDao;
+import vo.BoardVO;
+
+/**
+ * @author 김태호
+ * @version 1.0
+ * 
+ *          <p>
+ *          파일명 : BoardDaoImpl.java <br/>
+ *          설명 : 게시판 DAO 구현클래스 <br/>
+ * 
+ *          수정이력<br/>
+ *          --------------------------------------------<br/>
+ *          수정일자 |수정인|수정내용<br/>
+ *          --------------------------------------------<br/>
+ *          2017.01.24 김태호 최초생성<br/>
+ *          --------------------------------------------<br/>
+ *          </p>
+ */
+
+public class BoardDaoImpl implements BoardDao {
+
+	// 싱글톤 패턴을 사용하기 위한 자신 클래스의 인스턴스
+	private static BoardDaoImpl boardDaoImpl = new BoardDaoImpl();
+	
+
+	private BoardDaoImpl() {
+		
+	}
+
+	public static BoardDaoImpl getInstance() {
+		return boardDaoImpl;
+	}
+
+	@Override
+	public List<BoardVO> selectBoardList(SqlSession mapper, String boardTitle, String boardWriter) {
+		System.out.println("dao selectBoardList");
+		List<BoardVO> resultList = null;
+		BoardVO boardVO = new BoardVO();
+		boardVO.setBoardTitle(boardTitle);
+		boardVO.setBoardWriter(boardWriter);
+		
+		try {
+			resultList = mapper.selectList("test.selectTest", boardVO);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return resultList;
+	}
+
+	@Override
+	public BoardVO selectBoard(SqlSession mapper, String boardId) {
+		BoardVO vo = null;
+		BoardVO boardVO = new BoardVO();
+		boardVO.setBoardId(boardId);
+		System.out.println("boardId : " + boardId);
+		try {
+			vo = (BoardVO) mapper.selectOne("test.selectTest", boardVO);
+			System.out.println("vo :" + vo);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return vo;
+
+	}
+
+	@Override
+	public void insertBoard(SqlSession mapper, BoardVO vo) {
+		try {
+			System.out.println("insertTest : " + mapper.insert("test.insertTest", vo));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	@Override
+	public void updateBoard(SqlSession mapper, BoardVO vo) {
+
+		try {
+			System.out.println("updateTest : " + mapper.update("test.updateTest", vo));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void deleteBoard(SqlSession mapper, String boardId) {
+		BoardVO boardVO = new BoardVO();
+		boardVO.setBoardId(boardId);
+
+		try {
+			System.out.println("deleteTest : " +mapper.delete("test.deleteTest", boardVO));
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
