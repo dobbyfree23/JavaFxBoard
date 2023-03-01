0fb72cb8f15fe2f58d15fcc9641b936ed6703615
zahir <zahir7@naver.com>
2023-03-02 01:44:52 +0900

    BoardApp

--- src/application/Main.java
+++ src/application/Main.java
@@ -17,7 +17,7 @@ public class Main extends Application {
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene); 
 			
-			primaryStage.setTitle("태호 일기장!");
+			primaryStage.setTitle("BoardApp");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
